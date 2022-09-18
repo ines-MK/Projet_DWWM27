2f05b20bfb6dfcd2f440d78127321119f9a70b91
ines-MK <ines.mokktar@gmail.com>
2022-09-18 17:33:27 +0200

    baseAdmin.html.twig and admin/index.html.twig finished

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'admin' => 'AdminController',
         ]);
     }
 }
